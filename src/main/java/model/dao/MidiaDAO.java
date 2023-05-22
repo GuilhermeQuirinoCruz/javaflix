818095899ffc3628b9a2ad23c189ed8068cf8ebf
Bryan Ribeiro <bryanribeiro.bento@gmail.com>
2023-05-22 19:15:45 -0300

    Implementacao das Classes: Midia, MidiaDAO, MidiaDAOMysql e MidiaService, alem de atualizar as classes Database, DatabaseMysql com o metodo getMidiaDAO

---
+++ src/main/java/model/dao/MidiaDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao;
+
+/**
+ *
+ * @author bryan
+ */
+import java.util.ArrayList;
+import model.Midia;
+
+public abstract class MidiaDAO {
+    
+    private Midia midia;
+
+    public Midia getMidia() {
+        return this.midia;
+    }
+
+    public void setMidia(Midia midia) {
+        this.midia = midia;
+    }
+    
+    public abstract boolean Inserir();
+    public abstract boolean Atualizar();
+    public abstract boolean Excluir();
+    public abstract ArrayList<Midia> Listar();
+}
